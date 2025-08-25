70e834c53679a3830413b112f6bd8802a75e749c
CSidd <chandrachudsiddharth@gmail.com>
2025-08-25 15:07:45 +0530

    question solved

---
+++ traversing_array_elements.c
@@ -0,0 +1,11 @@
+//Program for traversing array elements.
+#include<stdio.h>
+
+int main(){
+    int arr[5]={1,2,3,4,5};
+    for (int i =0;i<5;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+}
